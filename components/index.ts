b00b3f240e426a3874861ae26fb3f9b68ce14508
shaheen7a <aousshaheen7@gmail.com>
2023-06-22 16:05:21 -0700

    Hero Section

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer
+}
